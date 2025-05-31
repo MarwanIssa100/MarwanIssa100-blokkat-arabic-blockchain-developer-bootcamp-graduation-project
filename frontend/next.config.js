41f9682bf32378b341d9ed52443d05eaf85c5f50
MarwanIssa100 <missa4312@gmail.com>
2025-05-31 16:58:29 +0300

    vercel fix

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // Enables static export
-  images: {
-    unoptimized: true, // Required for static export
-  },
   basePath: '/MarwanIssa100-blokkat-arabic-blockchain-developer-bootcamp-graduation-project',
   assetPrefix: '/MarwanIssa100-blokkat-arabic-blockchain-developer-bootcamp-graduation-project',
+  images: {
+    domains: ['vercel.com'],
+    unoptimized: true
+  }
 };
 
 module.exports = nextConfig; 
