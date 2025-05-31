68de251d5ee815b992d95a7459453d02e9208bd8
MarwanIssa100 <missa4312@gmail.com>
2025-05-31 13:34:09 +0300

    fix  publicClinet issues

--- frontend/hooks/useSafePublicClient.ts
+++ frontend/hooks/useSafePublicClient.ts
@@ -1,8 +1,8 @@
 import { usePublicClient } from 'wagmi'
-import { mainnet } from 'wagmi/chains'
+import { mainnet , scrollSepolia } from 'wagmi/chains'
 
 export function useSafePublicClient() {
-  const publicClient = usePublicClient({ chain: scrollSepolia })
+  const publicClient = usePublicClient({ chainId: scrollSepolia })
   if (!publicClient) {
     throw new Error('publicClient not available')
   }
